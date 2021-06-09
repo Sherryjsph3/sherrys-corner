7672de02572c43df39971df68f9141132f93b334
Sherry Joseph <sherryjoseph@sherrys-air.myfiosgateway.com>
2021-06-09 09:39:35 -0400

    css styling

--- models/productsSeed.js
+++ models/productsSeed.js
@@ -4,18 +4,18 @@ module.exports = [
       description: 'Recline into a unity of contemporary design and minimalist values with the Cloud Sofa. Perfect for relaxing with a book or a glass of wine on a calm summer night.',
       img: 'https://i.imgur.com/F4e5LOd.jpg',
       price: 400,
-      qty: 5
+      qty: 6
     }, {
-      name: 'Modern Linear Chandelier',
-      description: 'Linear Suspension. Conveniently adjustable look with a swiveling canopy, adjustable arms and rotating head',
-      img: 'https://i.imgur.com/2eMG9Bi.jpg',
+      name: 'Porcelain Ceramic Vase',
+      description: 'Adorable, mini vases are the trendy accents you need for table and shelf styling, like this neutral beige porcelain vase in a modern shape',
+      img: 'https://i.imgur.com/EkmKHjh.jpg',
       price: 75,
-      qty: 8
+      qty: 12
     }, {
       name: 'Full Length Standing Mirror',
       description: 'A sgreat addition to your bedroom or living room. It has a retractable stand that makes it easy to prop up ',
       img: 'https://i.imgur.com/u9gD27A.jpg',
       price: 150,
-      qty: 10
+      qty: 8
     }
   ]
